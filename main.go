4d8d4c99b659226a4109b1ee3a3e457de2c4a572
Adit0507 <adi05072002@gmail.com>
2024-08-27 14:01:26 +0530

    keeping track of goroutines

--- main.go
+++ main.go
@@ -3,13 +3,9 @@ package main
 import (
 	"fmt"
 	"log"
+	"maze-solver/internal/solver"
 	"os"
 )
-
-func usage() {
-	_, _ = fmt.Fprintln(os.Stderr,"Usage: maze-solver maze_10x10.png solution.png")
-	os.Exit(1)
-}	
 
 func main() {
 	if len(os.Args) != 3 {
@@ -20,4 +16,29 @@ func main() {
 	outputFile := os.Args[2]
 
 	log.Printf("Solving maze %q and saving it as %q", inputFile, outputFile)
+
+	s, err := solver.New(inputFile)
+	if err != nil {
+		exit(err)
+	}
+
+	err = s.Solve()
+	if err != nil {
+		exit(err)
+	}
+
+	err = s.SaveSolution(outputFile)
+	if err != nil {
+		exit(err)
+	}
 }
+
+func exit(err error) {
+	_, _ = fmt.Fprintf(os.Stderr, "Error: %s", err)
+	os.Exit(1)
+}
+
+func usage() {
+	_, _ = fmt.Fprintln(os.Stderr,"Usage: maze_solver input.png output.png")
+	os.Exit(1)
+}	
